cd9131f3941d1e5263070810b4fe78e39c51d607
Stas Bronevskiy <kototok903@gmail.com>
2025-05-13 22:56:25 -0500

    Remove unnecessary comments

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -8,26 +8,7 @@ export default function Resume() {
       <div className='container max-w-3xl'>
         <h1 className='title mb-4'>My resume</h1>
 
-        {/* <embed src="/public/documents/stas-resume.pdf" width="100%" height="1000px" /> */}
-        {/* <embed src="/documents/stas-resume.pdf#toolbar=0&scrollbar=0" width="100%" height="1000px" /> */}
-        {/* <iframe
-          src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://drive.google.com/uc?export=download&id=1O33_Zjj1Yxx7LieWRzUEKTnU9dK6rtd1#toolbar=0&scrollbar=0"
-          width="100%"
-          height="1000px"
-        ></iframe> */}
-        
-        {/* <iframe
-          src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://drive.google.com/uc?export=download&id=1O33_Zjj1Yxx7LieWRzUEKTnU9dK6rtd1"
-          width="100%"
-          height="1000px"
-        ></iframe> */}
-
-        {/* <iframe
-          src="https://drive.google.com/file/d/1O33_Zjj1Yxx7LieWRzUEKTnU9dK6rtd1/preview"
-          width="100%"
-          height="1000px"
-        ></iframe> */}
-
+        {/* Download button */}
         <div className="mb-4">
           <Link
             href="https://drive.google.com/uc?export=download&id=16xGqgVDR2kOtHW93LB6xWMO1aRzlvRjz"
@@ -39,7 +20,6 @@ export default function Resume() {
         </div>
         
         <PDFViewer pdfUrl="https://drive.google.com/file/d/16xGqgVDR2kOtHW93LB6xWMO1aRzlvRjz/preview" />
-
 
       </div>
     </section>
