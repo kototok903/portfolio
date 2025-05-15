8141a78bedfac8c62c0ffc0d38d7781f3b47c73a
Stas Bronevskiy <kototok903@gmail.com>
2025-05-14 23:59:53 -0500

    Add bg gradients

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Providers from "@/components/providers";
 import Header from "@/components/hearder";
 import Footer from "@/components/footer";
 import { Toaster } from "@/components/ui/sonner";
+import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +32,43 @@ export default function RootLayout({
     <body
       className={`${geistSans.variable} ${geistMono.variable} flex min-h-screen flex-col font-sans antialiased`}
     >
-      <Providers>
+      <Providers>        
+        <div className="fixed inset-0 -z-10 h-full w-full">
+          <BackgroundGradientAnimation 
+            gradientBackgroundStart="var(--color-background)"
+            gradientBackgroundEnd="var(--color-secondary)"
+            firstColor="var(--color-background)"
+            // secondColor="var(--color-background)"
+            // thirdColor="var(--color-background)"
+            // fourthColor="var(--color-background)"
+            // fifthColor="var(--color-background)"
+            // pointerColor="var(--color-background)"
+            
+            // secondColor="223, 94, 58"
+            // thirdColor="223, 94, 58"
+            // fourthColor="223, 94, 58"
+            // fifthColor="223, 94, 58"
+            // pointerColor="223 94 58"
+
+            // firstColor="transparent"
+            // secondColor="transparent"
+            // thirdColor="transparent"
+            // fourthColor="transparent"
+            // fifthColor="transparent"
+            // pointerColor="transparent"
+
+            // firstColor="var(--color-primary)"
+            // secondColor="var(--color-primary)"
+            thirdColor="var(--color-primary)"
+            fourthColor="var(--color-primary)"
+            // fifthColor="var(--color-primary)"
+            pointerColor="var(--color-primary)"
+
+            // interactive={true}
+            // blendingValue="difference"
+          />
+        </div>
+
         <Header />
         <main className='grow'>{children}</main>
         <Footer />
